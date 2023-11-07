6b9da53a155600c9ce42dc5cadd54caf83741193
kunal66666 <kunal.kashyap.fiem.cse19@teamfuture.in>
2023-11-07 20:13:19 +0530

    interview

---
+++ 1basicreact/src/App.jsx
@@ -0,0 +1,13 @@
+import Chai from "./chai"
+
+function App() {
+ 
+  return (
+    <>
+    <Chai/>
+   <h1>hello world</h1>
+
+   </>
+)  }
+
+export default App
---
+++ 1basicreact/src/Chai.jsx
@@ -0,0 +1,9 @@
+
+
+function Chai() {
+  return (
+    <div>chai</div>
+  )
+}
+
+export default Chai
